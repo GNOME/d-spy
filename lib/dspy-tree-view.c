7c4c42d930bae25fdb912fc16b8ebc28e9d7789a
Christian Hergert <chergert@redhat.com>
2019-04-17 13:38:23 -0700

    lib: expand extra rows like d-feet

--- lib/dspy-tree-view.c
+++ lib/dspy-tree-view.c
@@ -24,6 +24,7 @@
 
 #include <glib/gi18n.h>
 
+#include "dspy-private.h"
 #include "dspy-tree-view.h"
 
 G_DEFINE_TYPE (DspyTreeView, dspy_tree_view, GTK_TYPE_TREE_VIEW)
@@ -35,8 +36,57 @@ dspy_tree_view_new (void)
 }
 
 static void
+dspy_tree_view_row_expanded (GtkTreeView *view,
+                             GtkTreeIter *iter,
+                             GtkTreePath *path)
+{
+  DspyNode *node = NULL;
+  GtkTreeModel *model;
+
+  g_assert (GTK_IS_TREE_VIEW (view));
+  g_assert (iter != NULL);
+  g_assert (path != NULL);
+
+  if (GTK_TREE_VIEW_CLASS (dspy_tree_view_parent_class)->row_expanded)
+    GTK_TREE_VIEW_CLASS (dspy_tree_view_parent_class)->row_expanded (view, iter, path);
+
+  if (!(model = gtk_tree_view_get_model (view)) ||
+      !DSPY_IS_INTROSPECTION_MODEL (model))
+    return;
+
+  node = iter->user_data;
+
+  g_assert (node != NULL);
+  g_assert (DSPY_IS_NODE (node));
+
+  /* Expand children too if there are fixed number of children */
+  if (node->any.kind == DSPY_NODE_KIND_NODE ||    /* path node */
+      node->any.kind == DSPY_NODE_KIND_INTERFACE) /* iface node */
+    {
+      GtkTreeIter child;
+
+      if (gtk_tree_model_iter_children (model, &child, iter))
+        {
+          g_autoptr(GtkTreePath) copy = gtk_tree_path_copy (path);
+
+          gtk_tree_path_down (copy);
+
+          do
+            {
+              gtk_tree_view_expand_row (view, copy, FALSE);
+              gtk_tree_path_next (copy);
+            }
+          while (gtk_tree_model_iter_next (model, &child));
+        }
+    }
+}
+
+static void
 dspy_tree_view_class_init (DspyTreeViewClass *klass)
 {
+  GtkTreeViewClass *tree_view_class = GTK_TREE_VIEW_CLASS (klass);
+
+  tree_view_class->row_expanded = dspy_tree_view_row_expanded;
 }
 
 static void
