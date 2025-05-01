36ed86addca3c54000a3c1a218c2fdbde949470e
Christian Hergert <chergert@redhat.com>
2025-05-01 11:19:14 -0700

    window: add connections property
    This should make binding things easier from UI to use listview.

--- src/dspy-window.c
+++ src/dspy-window.c
@@ -20,6 +20,7 @@
 
 #include "config.h"
 
+#include "dspy-connection.h"
 #include "dspy-view.h"
 #include "dspy-window.h"
 
@@ -27,14 +28,66 @@ struct _DspyWindow
 {
   AdwApplicationWindow  parent_instance;
   DspyView             *view;
+  GListStore           *connections;
 };
 
-G_DEFINE_TYPE (DspyWindow, dspy_window, ADW_TYPE_APPLICATION_WINDOW)
+G_DEFINE_FINAL_TYPE (DspyWindow, dspy_window, ADW_TYPE_APPLICATION_WINDOW)
+
+enum {
+  PROP_0,
+  PROP_CONNECTIONS,
+  N_PROPS
+};
+
+static GParamSpec *properties[N_PROPS];
+
+static void
+dspy_window_dispose (GObject *object)
+{
+  DspyWindow *self = (DspyWindow *)object;
+
+  gtk_widget_dispose_template (GTK_WIDGET (self), DSPY_TYPE_WINDOW);
+
+  g_clear_object (&self->connections);
+
+  G_OBJECT_CLASS (dspy_window_parent_class)->dispose (object);
+}
+
+static void
+dspy_window_get_property (GObject    *object,
+                          guint       prop_id,
+                          GValue     *value,
+                          GParamSpec *pspec)
+{
+  DspyWindow *self = DSPY_WINDOW (object);
+
+  switch (prop_id)
+    {
+    case PROP_CONNECTIONS:
+      g_value_set_object (value, self->connections);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
 
 static void
 dspy_window_class_init (DspyWindowClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->dispose = dspy_window_dispose;
+  object_class->get_property = dspy_window_get_property;
+
+  properties[PROP_CONNECTIONS] =
+    g_param_spec_object ("connections", NULL, NULL,
+                         G_TYPE_LIST_MODEL,
+                         (G_PARAM_READABLE |
+                          G_PARAM_STATIC_STRINGS));
+
+  g_object_class_install_properties (object_class, N_PROPS, properties);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/dspy/dspy-window.ui");
   gtk_widget_class_bind_template_child (widget_class, DspyWindow, view);
@@ -45,6 +98,17 @@ dspy_window_class_init (DspyWindowClass *klass)
 static void
 dspy_window_init (DspyWindow *self)
 {
+  g_autoptr(DspyConnection) session = NULL;
+  g_autoptr(DspyConnection) system = NULL;
+
+  self->connections = g_list_store_new (DSPY_TYPE_CONNECTION);
+
+  system = dspy_connection_new_for_bus (G_BUS_TYPE_SYSTEM);
+  session = dspy_connection_new_for_bus (G_BUS_TYPE_SESSION);
+
+  g_list_store_append (self->connections, system);
+  g_list_store_append (self->connections, session);
+
   gtk_widget_init_template (GTK_WIDGET (self));
 
 #if DEVELOPMENT_BUILD
